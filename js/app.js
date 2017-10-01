b15e1f86147f8f570485a1819331532d5469bd5e
KarolakM <m@karolak.info>
2017-10-01 16:00:34 +0200

    Zadanie3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 $(function () {
-    $.ajax({
-        url: "http://localhost:8282/books",
-        type: "GET",
-        dataType: "json"
-    }).done(function (res) {
-        console.log(res)
-    });
+    var booksList = $("#books-list")
 
+    function loadAllBooks() {
+        $.ajax({
+            url: "http://localhost:8282/books",
+            type: "GET",
+            dataType: "json"
+        }).done(function (res) {
+            renderTitle(res)
+        });
+    }
 
+    loadAllBooks();
+
+    function renderTitle(arr) {
+        arr.forEach(function (element, index, array) {
+            var titleElement = $("<li>");
+            titleElement.text(element.title);
+            titleElement.appendTo(booksList);
+            var dir = $("<dir>");
+            dir.appendTo(booksList)
+        });
+    }
 });
